1e9eea2f1eace8c941b9c7cb12157aa0990f6402
jmjubaer <jmjubaer3927@gmail.com>
2025-03-02 21:36:07 +0600

    complete checkout page

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -5,15 +5,19 @@ import { TDeliveryAddress } from "../../types/order.type";
 import { useParams } from "react-router-dom";
 import { useGetBicycleByIdQuery } from "../../redux/features/product/productApi";
 import { FaCheckCircle, FaMinus, FaPlus } from "react-icons/fa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import surjopay from "../../assets/shurjoPay.png";
 import cod from "../../assets/cod.png";
+import { useCreateOrderMutation } from "../../redux/features/order/orderApi";
+import { useGetMeQuery } from "../../redux/features/auth/authApi";
 const Checkout = () => {
     const [quantity, setQuantity] = useState(1);
     const [paymentMethod, setPaymentMethod] = useState("");
     const { productId } = useParams();
     const { data: product } = useGetBicycleByIdQuery(productId);
-    console.log(productId, product);
+    const [createOrder] = useCreateOrderMutation();
+    const { data: currentUser } = useGetMeQuery(undefined);
+
     const {
         reset,
         register,
@@ -21,14 +25,48 @@ const Checkout = () => {
         formState: { errors },
     } = useForm<TDeliveryAddress>();
     const onSubmit: SubmitHandler<TDeliveryAddress> = async (data) => {
+        const toastId = toast.success("Order placing....");
+
         try {
-            console.log(data);
-            toast.success("Order placed successfully!");
-            reset();
+            if (!paymentMethod) {
+                return toast.error("Please select a payment method", {
+                    id: toastId,
+                });
+            }
+            const orderData = {
+                user: currentUser?.data?._id,
+                product: productId,
+                quantity,
+                totalPrice: product?.data?.price * quantity + 5,
+                paymentMethod,
+                deliveryAddress: {
+                    name: data.name,
+                    phoneNumber: data.phoneNumber,
+                    localAddress: data.localAddress,
+                    city: data.city,
+                    district: data.district,
+                    thana: data.thana,
+                    postalCode: Number(data.postalCode),
+                },
+            };
+            const result = await createOrder(orderData);
+            console.log(result);
+            if (result?.data?.success) {
+                toast.success("Order placed successfully!", { id: toastId });
+                reset();
+                if (result?.data?.data?.paymentUrl) {
+                    window.open(result?.data?.data?.paymentUrl, "_self");
+                }
+            }
         } catch (error: any) {
-            toast.error(error.message);
+            toast.error(error.message, { id: toastId });
         }
     };
+    useEffect(() => {
+        if (quantity == 0) {
+            setQuantity(1);
+        }
+    }, [quantity]);
     return (
         <form
             onSubmit={handleSubmit(onSubmit)}
@@ -191,7 +229,11 @@ const Checkout = () => {
                             <input
                                 type='number'
                                 min='1'
-                                value={quantity}
+                                onChange={(e) =>
+                                    setQuantity(Number(e.target.value))
+                                }
+                                // defaultValue={quantity}
+                                value={quantity || 1}
                                 className='input_primary w-40'
                             />
                             <button
@@ -205,45 +247,47 @@ const Checkout = () => {
                 </div>
                 <div className='overflow-hidden rounded-lg border mt-5 border-black/10 '>
                     <table className='w-full border border-black/10'>
-                        <tr className='border border-black/10 bg-black/10'>
-                            <th
-                                colSpan={2}
-                                className='border text-lg border-black/10 secondary_font p-2 px-3'>
-                                Order Summary
-                            </th>
-                        </tr>
-                        <tr className='border border-black/10'>
-                            <td className='border w-1/2 border-black/10 p-2 px-3'>
-                                Price
-                            </td>
-                            <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
-                                ${product?.data?.price}
-                            </td>
-                        </tr>
-                        <tr className='border border-black/10'>
-                            <td className='border w-1/2 border-black/10 p-2 px-3'>
-                                Quantity
-                            </td>
-                            <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
-                                {quantity}
-                            </td>
-                        </tr>
-                        <tr className='border border-black/10'>
-                            <td className='border w-1/2 border-black/10 p-2 px-3'>
-                                Shipping
-                            </td>
-                            <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
-                                ${"5"}
-                            </td>
-                        </tr>
-                        <tr className='border border-black/10'>
-                            <td className='border font-bold w-1/2 border-black/10 p-2 px-3'>
-                                Total
-                            </td>
-                            <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
-                                ${(product?.data?.price * quantity) + 5}
-                            </td>
-                        </tr>
+                        <tbody>
+                            <tr className='border border-black/10 bg-black/10'>
+                                <th
+                                    colSpan={2}
+                                    className='border text-lg border-black/10 secondary_font p-2 px-3'>
+                                    Order Summary
+                                </th>
+                            </tr>
+                            <tr className='border border-black/10'>
+                                <td className='border w-1/2 border-black/10 p-2 px-3'>
+                                    Price
+                                </td>
+                                <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
+                                    ${product?.data?.price}
+                                </td>
+                            </tr>
+                            <tr className='border border-black/10'>
+                                <td className='border w-1/2 border-black/10 p-2 px-3'>
+                                    Quantity
+                                </td>
+                                <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
+                                    {quantity}
+                                </td>
+                            </tr>
+                            <tr className='border border-black/10'>
+                                <td className='border w-1/2 border-black/10 p-2 px-3'>
+                                    Shipping
+                                </td>
+                                <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
+                                    ${"5"}
+                                </td>
+                            </tr>
+                            <tr className='border border-black/10'>
+                                <td className='border font-bold w-1/2 border-black/10 p-2 px-3'>
+                                    Total
+                                </td>
+                                <td className='border w-1/2 font-bold border-black/10 p-2 px-3'>
+                                    ${product?.data?.price * quantity + 5}
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
 
@@ -276,9 +320,7 @@ const Checkout = () => {
                     </button>
                 </div>
 
-                <button
-                    type='submit'
-                    className='button_primary_md w-full mt-5'>
+                <button type='submit' className='button_primary_md w-full mt-5'>
                     Order Now
                 </button>
             </div>
